df4219f7ebc395abef9d56968815533a844eb9d7
Emil Ohlsson <kotte@kottland.net>
2017-12-03 13:32:16 +0100

    part 2

--- 2017/03-spiral-memory/src/main.rs
+++ 2017/03-spiral-memory/src/main.rs
@@ -1,22 +1,34 @@
+use std::collections::HashMap;
+
 const ADDRESS: usize = 277678;
 
-fn spiral(addr: usize) -> (isize, isize) {
+fn spiral(addr: usize) -> usize {
     let mut p = (0, 0);
     let mut dp = (0, -1);
 
-    for _ in 1..addr {
+    let mut values: HashMap<(isize, isize), usize> = HashMap::new();
+    
+    values.insert((0,0), 1);
+
+    loop {
         if p.0 == p.1 || (p.0 < 0 && p.0 == -p.1) || (p.0 > 0 && p.0 == 1 - p.1) {
             dp = (-dp.1, dp.0);
         }
         p = (p.0 + dp.0, p.1 + dp.1);
+        let val = 
+            values.get(&(p.0 - 1, p.1)).unwrap_or(&0) +
+            values.get(&(p.0 - 1, p.1 -1)).unwrap_or(&0) +
+            values.get(&(p.0, p.1 - 1)).unwrap_or(&0) +
+            values.get(&(p.0 + 1, p.1 - 1)).unwrap_or(&0) +
+            values.get(&(p.0 + 1, p.1)).unwrap_or(&0) +
+            values.get(&(p.0 + 1, p.1 + 1)).unwrap_or(&0) +
+            values.get(&(p.0, p.1 + 1)).unwrap_or(&0) +
+            values.get(&(p.0 - 1, p.1 + 1)).unwrap_or(&0);
+        if val >= addr { return val; }
+        values.insert(p, val);
     }
-    return p;
-}
-
-fn manhattan_distance(p: (isize, isize)) -> usize {
-    (p.0.abs() + p.1.abs()) as usize
 }
 
 fn main() {
-    println!("{}", manhattan_distance(spiral(ADDRESS)));
+    println!("{}", spiral(ADDRESS));
 }
