9d2df3debdb0d3a2d7f6616501dd7a97c5f2be84
Emil Ohlsson <emil@kottland.net>
2017-12-09 18:35:24 +0100

    Part 1

---
+++ 2017/09-stream-processing/src/main.rs
@@ -0,0 +1,62 @@
+#[derive(Clone,PartialEq,Eq)]
+enum Kind {
+    Group,
+    Garbage,
+}
+
+fn score_stream(stream: &str) -> usize {
+    let mut state = Kind::Group;
+    let mut score = 0;
+    let mut depth = 0;
+    let mut escaped = false;
+
+    for ch in stream.chars() {
+        if state == Kind::Garbage && escaped {
+            escaped = false;
+            continue;
+        }
+        state = match state {
+            Kind::Group => {
+                match ch {
+                    '{' => {
+                        depth += 1;
+                        Kind::Group
+                    },
+                    '}' => {
+                        score += depth;
+                        depth -= 1;
+                        Kind::Group
+                    },
+                    '<' => Kind::Garbage,
+                    ',' => Kind::Group,
+                    _ => panic!("Unexpected {}", ch),
+                }
+            },
+            Kind::Garbage => {
+                match ch {
+                '>' => Kind::Group,
+                '!' => {escaped = true; Kind::Garbage},
+                _ => Kind::Garbage,
+                }
+            },
+        }
+    }
+    score
+}
+
+fn main() {
+    let input_score = score_stream(include_str!("input.txt").trim());
+    println!("{}", input_score);
+}
+
+#[test]
+fn test_inputs() {
+    assert_eq!(score_stream("{}"), 1);
+    assert_eq!(score_stream("{{{}}}"), 6);
+    assert_eq!(score_stream("{{},{}}"), 5);
+    assert_eq!(score_stream("{{{},{},{{}}}}"), 16);
+    assert_eq!(score_stream("{<a>,<a>,<a>,<a>}"), 1);
+    assert_eq!(score_stream("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
+    assert_eq!(score_stream("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
+    assert_eq!(score_stream("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
+}
