57ef52f447e90743a9bb12385a7e956533d8a260
Emil Ohlsson <emil@kottland.net>
2017-12-13 23:38:07 +0100

    Part 2

--- 2017/13-packet-scanner/src/main.rs
+++ 2017/13-packet-scanner/src/main.rs
@@ -1,4 +1,3 @@
-
 fn parse_input(desc: &str) -> Vec<(isize, isize)> {
     desc.lines().map(|l| {
         let ts: Vec<isize> = l.split(':').map(|t| t.trim().parse::<isize>().unwrap()).collect();
@@ -6,36 +5,27 @@ fn parse_input(desc: &str) -> Vec<(isize, isize)> {
     }).collect()
 }
 
-fn simulate(firewall: Vec<(isize, isize)>) -> isize {
-    let mut scanners: Vec<isize> = vec![0; firewall.len()];
-    let mut scanner_step: Vec<isize> = vec![0; firewall.len()];
-    let mut severity = 0;
+fn severity(firewall: &Vec<(isize, isize)>) -> isize {
+    firewall.iter().filter(|&&(position, height)| {
+        position % (2 * (height - 1)) == 0
+    }).map(|&(position, height)| position * height).sum()
+}
 
-    let levels = firewall.iter().map(|l| l.0).max().unwrap() + 1;
-
-    for level in 0..levels {
-        for i in 0..scanners.len() {
-            // Step and change direction if at end of layer
-            scanners[i] = scanners[i] + scanner_step[i];
-            if scanners[i] == 0 { scanner_step[i] = 1;}
-            if scanners[i] == firewall[i].1 - 1 { scanner_step[i] = -1;}
-            
-            // Check if caught
-            if level == firewall[i].0 && scanners[i] == 0 {
-                severity += firewall[i].0 * firewall[i].1;
-            }
-        }
-    }
-    return severity;
+fn safe_delay(firewall: &Vec<(isize, isize)>) -> isize {
+    (0..)
+        .find(|w| !firewall.iter().any(|&(p, h)| (p + w) % (2 * (h - 1)) == 0))
+        .unwrap()
 }
 
 fn main() {
     let input = parse_input(include_str!("input.txt").trim());
-    println!("{}", simulate(input));
+    println!("{}", severity(&input));
+    println!("{}", safe_delay(&input));
 }
 
 #[test]
 fn test_simple() {
     let input = parse_input(include_str!("input-simple.txt").trim());
-    assert_eq!(simulate(input), 24);
+    assert_eq!(severity(&input), 24);
+    assert_eq!(safe_delay(&input), 10);
 }
