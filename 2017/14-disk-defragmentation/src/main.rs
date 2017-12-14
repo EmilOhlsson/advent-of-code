fdd34bbc0a27cbb45c5dff66c721e5c305ad0f15
Emil Ohlsson <kotte@kottland.net>
2017-12-14 20:40:08 +0100

    Part 2

--- 2017/14-disk-defragmentation/src/main.rs
+++ 2017/14-disk-defragmentation/src/main.rs
@@ -1,4 +1,6 @@
-fn knot_hash(input_str: &str) -> Vec<usize> {
+use std::collections::HashSet;
+
+fn knot_hash(input_str: &str) -> Vec<u8> {
 	let mut input = input_str.as_bytes().iter().map(|&b| b as usize).collect::<Vec<usize>>();
 	let length = 256;
 	let mut skip = 0;
@@ -19,25 +21,67 @@ fn knot_hash(input_str: &str) -> Vec<usize> {
 			skip += 1;
 		}
 	}
-	return codes.chunks(16).map(|ch| ch.iter().fold(0,|a, &v| a ^ v)).collect::<Vec<usize>>();
+	return codes.chunks(16).map(|ch| ch.iter().fold(0,|a, &v| a as u8 ^ v as u8)).collect::<Vec<u8>>();
 }
 
-fn fragmentation_count(input_str: &str) -> usize {
-    let mut count = 0usize;
+fn get_rc(input: &Vec<Vec<bool>>, r: isize, c: isize) -> bool {
+    if let Some(ref line) = input.get(r as usize) {
+        if let Some(&active) = line.get(c as usize) {
+            return active;
+        }
+    }
+    return false;
+}
+
+fn visit(visited: &mut HashSet<(isize, isize)>, input: &Vec<Vec<bool>>, r: isize, c: isize) {
+    if visited.contains(&(r, c)) { return; }
+
+    visited.insert((r,c));
+    if get_rc(input, r - 1, c) {visit(visited, input, r - 1, c)}
+    if get_rc(input, r, c - 1) {visit(visited, input, r, c - 1)}
+    if get_rc(input, r + 1, c) {visit(visited, input, r + 1, c)}
+    if get_rc(input, r, c + 1) {visit(visited, input, r, c + 1)}
+}
+
+fn count_groups(input: &Vec<Vec<bool>>) -> usize {
+    let mut groups = 0;
+    let mut visited: HashSet<(isize, isize)> = HashSet::new();
+    for r in 0..128isize {
+        for c in 0..128isize {
+            if !input[r as usize][c as usize] || visited.contains(&(r, c)) { continue; }
+            groups += 1;
+            visit(&mut visited, input, r, c);
+        }
+    }
+
+    return groups;
+}
+
+fn group_count(input_str: &str) -> usize {
+    let mut layout: Vec<Vec<bool>> = Vec::new();
     for i in 0..128 {
         let lcode = format!("{}-{}", input_str, i);
         let linehash = knot_hash(&lcode);
-        count += linehash.iter().map(|v| v.count_ones() as usize).sum::<usize>();
+        let mut line: Vec<bool> = Vec::new();
+        linehash.iter().for_each(|v| {
+            line.append(&mut (0..8).map(|j| v & (1 << (8 - j - 1)) != 0).collect::<Vec<bool>>());
+        });
+        layout.push(line);
     }
-    return count;
+
+    for l in layout.iter() {
+        println!("{}", l.iter().map(|&x| if x { '#' } else { ' ' }).collect::<String>());
+    }
+    return count_groups(&layout);
 }
 
 fn main() {
     let input = "nbysizxe";
-    println!("{}", fragmentation_count(&input));
+    println!("{}", group_count(&input));
 }
 
 #[test]
 fn test_count() {
-	assert_eq!(fragmentation_count("flqrgnkx"), 8108);
+    println!();
+	assert_eq!(group_count("flqrgnkx"), 1242);
 }
