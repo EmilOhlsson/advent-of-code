95e0eed33bad1b0bac5b8f5c2ba4043016fac722
Emil Ohlsson <kotte@kottland.net>
2017-12-15 07:30:38 +0100

    Part 2

--- 2017/15-dueling-generators/src/main.rs
+++ 2017/15-dueling-generators/src/main.rs
@@ -1,8 +1,14 @@
 fn count_pairs(mut a: usize, mut b: usize) -> usize {
     let mut count = 0;
-    for _ in 0..40_000_000 {
-        a = (a *16807) % 2147483647;
-        b = (b * 48271) % 2147483647;
+    for _ in 0..5_000_000 {
+        while {
+            a = a *16807 % 2147483647;
+            a % 4 != 0
+        } {}
+        while { 
+            b = b * 48271 % 2147483647;
+            b % 8 != 0
+        } {}
 
         if (a & 0xffff) == (b & 0xffff) { count += 1;}
     }
@@ -16,5 +22,5 @@ fn main() {
 
 #[test]
 fn test_generators() {
-    assert_eq!(count_pairs(65, 8921), 588);
+    assert_eq!(count_pairs(65, 8921), 309);
 }
