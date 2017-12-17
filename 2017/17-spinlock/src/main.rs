73760558b3db4a32276aacc4c8ab7fdc1bcf9bf5
Emil Ohlsson <emil@kottland.net>
2017-12-17 12:03:50 +0100

    Part 2

--- 2017/17-spinlock/src/main.rs
+++ 2017/17-spinlock/src/main.rs
@@ -1,18 +1,18 @@
-const INSERTIONS: usize = 2017;
+const INSERTIONS: usize = 50_000_000;
 
-fn solve(count: usize) -> (usize, Vec<usize>) {
-    let mut rbuffer = vec![0];
+fn solve(count: usize) -> usize {
     let mut i = 0;
+    let mut first = None;
     for v in 1..(INSERTIONS + 1) {
-        i = (i + count) % rbuffer.len() + 1;
-        rbuffer.insert(i, v);
+        i = (i + count) % v + 1;
+        if i == 1 { first = Some(v); }
     }
-    (i, rbuffer)
+    return first.unwrap();
 }
 
 fn main() {
-    let (i, v) = solve(359);
-    println!("{}", v[i + 1]);
+    let v = solve(359);
+    println!("{}", v);
 }
 
 #[test]
