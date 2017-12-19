105ddc6d6719526b6da5a5f3392ba2b9d18a183c
Emil Ohlsson <emil@kottland.net>
2017-12-19 21:08:30 +0100

    Part 1

---
+++ 2017/19-series-of-tubes/src/main.rs
@@ -0,0 +1,81 @@
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum Direction {
+	Up, Down, Left, Right
+}
+
+fn get_string(input: &str) -> String {
+	let mut matrix: Vec<Vec<char>> = Vec::new();
+	for line in input.lines() {
+		let mut l: Vec<char> = Vec::new();
+		for ch in line.chars() {
+			l.push(ch);
+		}
+		matrix.push(l);
+	}
+
+	let mut dir = Direction::Down;
+	let mut r = 0;
+	let mut c = 0;
+
+	// Find entry
+	for i in 0.. {
+		if matrix[0][i] == '|' {
+			c = i;
+			break;
+		}
+	}
+
+	let mut result = String::new();
+	loop {
+		let (rn, cn) = match dir {
+			Direction::Up => (r - 1, c),
+			Direction::Down => (r + 1, c),
+			Direction::Left => (r, c - 1),
+			Direction::Right => (r, c + 1),
+		};
+		r = rn;
+		c = cn;
+		match matrix[r][c] {
+			'-'|'|' => (),
+			'+' => {
+				if dir == Direction::Up || dir == Direction::Down {
+					if *matrix[r].get(c - 1).unwrap_or(&' ') ==  ' ' {
+						dir = Direction::Right;
+					} else {
+						dir = Direction::Left;
+					}
+				} else {
+					if let Some(ref row) = matrix.get(r - 1) {
+						if row[c] == ' ' {
+							dir = Direction::Down;
+						}
+					} else {
+						dir = Direction::Down;
+					}
+					if let Some(ref row) = matrix.get(r + 1) {
+						if row[c] == ' ' {
+							dir = Direction::Up;
+						}
+					} else {
+						dir = Direction::Up;
+					}
+				}
+			}
+			' ' => break,
+			_ => result.push(matrix[r][c]),
+		}
+	}
+
+	return result;
+}
+
+fn main() {
+	let input = include_str!("input.txt");
+	println!("{}", get_string(input));
+}
+
+#[test]
+fn test_get_string() {
+	let input = include_str!("input-simple.txt");
+	assert_eq!(get_string(input), "ABCDEF");
+}
