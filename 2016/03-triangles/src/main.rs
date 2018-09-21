f29e8a44d5badf4bbe45f244641dbee303c85917
Emil Ohlsson <kotte@kottland.net>
2018-09-21 19:19:34 +0200

    Solve 2016-03-p2

--- 2016/03-triangles/src/main.rs
+++ 2016/03-triangles/src/main.rs
@@ -1,36 +1,40 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
-
-fn verify_triangle(a: usize, b: usize, c: usize) -> bool {
+fn valid_triangles(a: usize, b: usize, c: usize) -> bool {
     (a + b > c) && (c + b > a) && (a + c > b)
 }
 
 #[test]
 fn impossible() {
-    assert_eq!(verify_triangle(5, 10, 25), false);
+    assert_eq!(valid_triangles(5, 10, 25), false);
 }
 
 #[test]
 fn simple() {
-    assert_eq!(verify_triangle(1, 1, 1), true);
+    assert_eq!(valid_triangles(1, 1, 1), true);
 }
 
 fn main() {
-    let reader = match File::open("input.txt") {
-        Ok(file) => BufReader::new(file),
-        Err(e) => panic!("Unable to open file: {}", e),
-    };
+    let input = include_str!("../input.txt");
 
-    let mut valid_triangles: usize = 0;
-    for l in reader.lines() {
-        let line = l.unwrap();
-        let toks: Vec<&str> = line.split_whitespace().collect();
-        let a = toks[0].parse::<usize>().unwrap();
-        let b = toks[1].parse::<usize>().unwrap();
-        let c = toks[2].parse::<usize>().unwrap();
-
-        valid_triangles += if verify_triangle(a, b, c) {1} else {0}
+    let mut count: usize = 0;
+    let triangles: Vec<Vec<usize>> = input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .map(|t| t.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        }).collect();
+    for i in 0..(triangles.len() / 3) {
+        for j in 0..3 {
+            count += if valid_triangles(
+                triangles[i * 3][j],
+                triangles[i * 3 + 1][j],
+                triangles[i * 3 + 2][j],
+            ) {
+                1
+            } else {
+                0
+            }
+        }
     }
-    println!("Found {} valid triangles", valid_triangles);
+    println!("{}", count);
 }
