7eeddb35d2957379275207d7b2357ece81d632c6
Emil Ohlsson <emil@kottland.net>
2018-12-03 18:28:51 +0100

    Solve 2018-03-p2

--- 2018/03-no-matter-how-you-slice-it/src/main.rs
+++ 2018/03-no-matter-how-you-slice-it/src/main.rs
@@ -1,15 +1,14 @@
 extern crate regex;
 
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 struct Claim {
-    id: usize,
     pos: (usize, usize),
     size: (usize, usize),
 }
 
-fn solve(input: &str) -> usize {
+fn solve(input: &str) -> (usize, Vec<usize>) {
     let re = Regex::new(r"(\d+)").unwrap();
     let claims = input
         .lines()
@@ -18,27 +17,41 @@ fn solve(input: &str) -> usize {
                 .captures_iter(l)
                 .map(|c| c[1].parse::<usize>().unwrap())
                 .collect::<Vec<usize>>();
-            Claim {
-                id: ts[0],
-                pos: (ts[1], ts[2]),
-                size: (ts[3], ts[4]),
-            }
-        }).collect::<Vec<Claim>>();
+            (
+                ts[0],
+                Claim {
+                    pos: (ts[1], ts[2]),
+                    size: (ts[3], ts[4]),
+                },
+            )
+        }).collect::<HashMap<usize, Claim>>();
 
-    let mut fabric: HashMap<(usize, usize), usize> = HashMap::new();
-    for claim in &claims {
+    let mut fabric: HashMap<(usize, usize), Vec<usize>> = HashMap::new();
+
+    let mut overlaps: HashSet<usize> = HashSet::new();
+    let ids: HashSet<usize> = claims.keys().map(|i| *i).collect();
+    for (id, claim) in &claims {
         for y in claim.pos.1..(claim.pos.1 + claim.size.1) {
             for x in claim.pos.0..(claim.pos.0 + claim.size.0) {
-                let cls = fabric.entry((x, y)).or_insert(0);
-                *cls += 1;
+                let ids = fabric.entry((x, y)).or_insert(Vec::new());
+                ids.push(*id);
+                if ids.len() > 1 {
+                    for id in ids {
+                        overlaps.insert(*id);
+                    }
+                }
             }
         }
     }
 
-    fabric.values().filter(|&v| *v > 1).count()
+    (
+        fabric.values().filter(|&v| v.len() > 1).count(),
+        ids.difference(&overlaps).map(|i| *i).collect(),
+    )
 }
 
 fn main() {
     let input = include_str!("input.txt");
-    println!("{}", solve(input));
+    let (p1, p2) = solve(input);
+    println!("{} - {:?}", p1, p2);
 }
