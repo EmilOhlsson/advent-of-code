f37b28ca3d7ae0a1e84c15f0f6e0216039a383b8
Emil Ohlsson <emil@kottland.net>
2018-12-19 08:28:31 +0100

    Solve 2018-16-p1,2

--- 2018/16-chronal-classification/src/main.rs
+++ 2018/16-chronal-classification/src/main.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+use regex::Regex;
 
 fn addr(regs: &mut Vec<usize>, a: usize, b: usize, c: usize) -> Result<(), ()> {
     regs[c] = regs.get(a).ok_or(())? + regs.get(b).ok_or(())?;
@@ -88,7 +90,13 @@ fn eqrr(regs: &mut Vec<usize>, a: usize, b: usize, c: usize) -> Result<(), ()> {
     Ok(())
 }
 
-fn solve_p1(input: &str, ip_reg: usize) -> usize {
+struct TestCase {
+    before: Vec<usize>,
+    inst: Vec<usize>,
+    after: Vec<usize>,
+}
+
+fn solve(input: &str, code: &str) -> (usize, usize) {
     let mut isa: HashMap<String, Box<Fn(&mut Vec<usize>, usize, usize, usize) -> Result<(), ()>>> =
         HashMap::new();
     isa.insert("addr".to_owned(), Box::new(addr));
@@ -108,42 +116,71 @@ fn solve_p1(input: &str, ip_reg: usize) -> usize {
     isa.insert("eqrr".to_owned(), Box::new(eqrr));
     isa.insert("eqri".to_owned(), Box::new(eqri));
 
-    let program: Vec<(String, usize, usize, usize)> = input
-        .lines()
-        .map(|l| {
-            let toks = l.split_whitespace().collect::<Vec<&str>>();
-            (
-                toks[0].to_owned(),
-                toks[1].parse::<usize>().unwrap(),
-                toks[2].parse::<usize>().unwrap(),
-                toks[3].parse::<usize>().unwrap(),
-            )
+    let re = Regex::new(
+        r"(?m)^Before: \[(\d+), (\d+), (\d+), (\d+)\]$
+^(\d+) (\d+) (\d+) (\d+)$
+^After:  \[(\d+), (\d+), (\d+), (\d+)\]$",
+    )
+    .unwrap();
+
+    let test_cases = re
+        .captures_iter(input)
+        .map(|cap| {
+            let vs = cap
+                .iter()
+                .skip(1)
+                .map(|n| n.unwrap().as_str().parse::<usize>().unwrap())
+                .collect::<Vec<usize>>();
+
+            TestCase {
+                before: vs[0..4].to_vec(),
+                inst: vs[4..8].to_vec(),
+                after: vs[8..12].to_vec(),
+            }
         })
-        .collect();
+        .collect::<Vec<TestCase>>();
 
-    let mut registers = vec![1;6];
-    registers[ip_reg] = 0;
+    let mut count = 0;
+    let mut known: HashMap<usize, String> = HashMap::new();
 
-    loop {
-        let ip = registers[ip_reg];
-        if let Some ((inst, a, b, c)) = program.get(ip) {
-            println!("ip={} {:?} -- {} {} {} {}", ip, registers, inst, a, b, c);
-            let instr = isa.get(inst).unwrap();
-            instr(&mut registers, *a, *b, *c).unwrap();
-        } else {
-            return registers[0];
+    /* Build op code table */
+    for case in &test_cases {
+        let mut candidates = isa.iter().filter_map(|(n, f)| {
+            let mut regs = case.before.clone();
+            if f(&mut regs, case.inst[1], case.inst[2], case.inst[3]).is_ok() && regs == case.after {
+                Some(n.to_owned())
+            } else {
+                None
+            }
+        }).collect::<HashSet<String>>();
+
+        if candidates.len() >= 3 {
+            count += 1;
         }
-        registers[ip_reg] += 1;
+
+        for k in known.values() {
+            candidates.remove(k);
+        }
+
+        if candidates.len() == 1 {
+            println!("OP code {} is {:?}", case.inst[0], candidates);
+            known.insert(case.inst[0], candidates.iter().next().unwrap().to_string());
+        }
     }
+
+    /* Run code */
+    let mut registers = vec![0;4];
+    for l in code.lines() {
+        let vs = l.split_whitespace().map(|t| t.parse::<usize>().unwrap()).collect::<Vec<_>>();
+        let exec = isa.get(known.get(&vs[0]).unwrap()).unwrap();
+        assert!(exec(&mut registers, vs[1], vs[2], vs[3]).is_ok());
+    }
+
+    (count, registers[0])
 }
 
 fn main() {
-    let input = include_str!("input.txt");
-    println!("{}", solve_p1(input, 2));
-}
-
-#[test]
-fn test() {
-    let input = include_str!("input-simple.txt");
-    assert_eq!(solve_p1(input, 0), 7);
+    let samples = include_str!("input-samples.txt");
+    let code = include_str!("input-code.txt");
+    println!("{:?}", solve(samples, code));
 }
