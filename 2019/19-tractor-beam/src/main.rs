14fcebb77f0c7d2463bb560f2735ee5c8531f236
Emil Ohlsson <emil@kottland.net>
2019-12-24 12:27:03 +0100

    Solve 2019-19-p2

--- 2019/19-tractor-beam/src/main.rs
+++ 2019/19-tractor-beam/src/main.rs
@@ -7,18 +7,31 @@ fn solve(input: &str) -> u32 {
     for y in 0..50 {
         for x in 0..50 {
             if machine.clone().run(&[x, y])[0] > 0 {
-                print!("#");
                 sum += 1;
-            } else {
-                print!(".");
             }
         }
-        println!();
     }
     sum
+}
+
+fn solve_v2(input: &str) -> i64 {
+    let machine = Intmachine::load(input, 0);
+    for y in 1_000.. {
+        for x in 3 * y / 5.. {
+            if machine.clone().run(&[x, y])[0] > 0 {
+                if machine.clone().run(&[x + 99, y - 99])[0] > 0 {
+                    return x * 10_000 + y - 99;
+                }
+                break;
+            }
+        }
+    }
+
+    panic!()
 }
 
 fn main() {
     let input = include_str!("input");
     println!("{}", solve(input));
+    println!("{}", solve_v2(input));
 }
