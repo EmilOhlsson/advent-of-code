edd7981e9237f1baf0089bf56f8943b59bd2b2e9
Emil Ohlsson <emil@kottland.net>
2020-01-03 16:12:13 +0100

    Solve 2019-22-p2

--- 2019/22-slam-shuffle/src/main.rs
+++ 2019/22-slam-shuffle/src/main.rs
@@ -1,3 +1,6 @@
+use num::bigint::BigInt;
+use num::bigint::ToBigInt;
+use num::traits::{One, Zero};
 use regex::Regex;
 
 fn wrap(v: i32, r: i32) -> i32 {
@@ -44,7 +47,64 @@ fn solve(input: &str, size: i32) -> i32 {
     modulo(lo * 2019 + hi, size)
 }
 
+fn polypow(a: BigInt, b: BigInt, m: BigInt, n: BigInt) -> (BigInt, BigInt) {
+    if m == Zero::zero() {
+        (One::one(), Zero::zero())
+    } else if m.clone() % 2 == Zero::zero() {
+        polypow(
+            a.clone() * a.clone() % n.clone(),
+            (a.clone() * b.clone() + b.clone()) % n.clone(),
+            m.clone() / 2,
+            n.clone(),
+        )
+    } else {
+        let (c, d) = polypow(a.clone(), b.clone(), m.clone() - 1, n.clone());
+        (a.clone() * c % n.clone(), (a.clone() * d + b) % n)
+    }
+}
+
+fn solve_v2(input: &str, size: BigInt, times: BigInt, card: BigInt) -> BigInt {
+    let cut_re = Regex::new(r"cut (-?\d+)").unwrap();
+    let deal_re = Regex::new(r"deal into new stack").unwrap();
+    let incr_re = Regex::new(r"deal with increment (-?\d+)").unwrap();
+
+    // store as y = k * x + m as k and m
+    let mut k: BigInt = One::one();
+    let mut m: BigInt = Zero::zero();
+
+    // Parse the lines in revers, and translate to inverse
+    // of rule
+    for line in input.lines().rev() {
+        if let Some(cut) = cut_re.captures(line) {
+            let v = cut[1].parse::<BigInt>().unwrap();
+            m = (m + v) % size.clone();
+        } else if let Some(incr) = incr_re.captures(line) {
+            let v = incr[1].parse::<BigInt>().unwrap();
+            let vp = v.modpow(&(size.clone() - 2.to_bigint().unwrap()), &size);
+            k = k * vp.clone() % size.clone();
+            m = m * vp.clone() % size.clone();
+        } else if deal_re.captures(line).is_some() {
+            k = -k;
+            m = size.clone() - m - 1
+        } else {
+            panic!("line doesn't match: {:?}", line);
+        }
+    }
+
+    let (kp, mp) = polypow(k, m, times, size.clone());
+    (card * kp + mp) % size
+}
+
 fn main() {
     let input = include_str!("input");
     println!("{}", solve(input, 10_007));
+    println!(
+        "{}",
+        solve_v2(
+            input,
+            119_315_717_514_047i64.to_bigint().unwrap(),
+            101_741_582_076_661i64.to_bigint().unwrap(),
+            2_020i128.to_bigint().unwrap()
+        )
+    );
 }
