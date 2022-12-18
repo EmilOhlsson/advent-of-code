4d07f93f2ea42a6f22e6d7c550ef88329bca8579
Emil Ohlsson <kotte@kottland.net>
2022-12-18 18:39:24 +0100

    Solve 2022-16

--- lua/advent-of-code/2022/16.lua
+++ lua/advent-of-code/2022/16.lua
@@ -24,23 +24,6 @@ local function parse(lines)
     return valves, tunnels
 end
 
-local function set_to_to_string(set)
-    local list = {}
-    for x, s in pairs(set) do
-        if s then
-            table.insert(list, x)
-        end
-    end
-    local result = "["
-    if #list > 0 then
-        result = result .. list[1]
-        for i = 2, #list do
-            result = result .. ',' .. list[i]
-        end
-    end
-    return result .. ']'
-end
-
 local function calculate_distances(tunnels)
     local distances = {}
     for start, _ in pairs(tunnels) do
@@ -77,7 +60,8 @@ local function get_nonzero_valves(valves)
 end
 
 -- Copy and return table, except `except`, if provided
-local function copy_table(tbl, except)
+function table.copye(tbl, except)
+    assert(type(tbl) == 'table')
     local tbl_copy = {}
     for x, y in pairs(tbl) do
         tbl_copy[x] = y
@@ -103,7 +87,7 @@ function M.solve_p1(lines)
                 local destination_score = released + (30 - time) * rate
                 score = math.max(score, destination_score)
             else
-                local destinations_new = copy_table(destinations, destination)
+                local destinations_new = table.copye(destinations, destination)
                 local destination_score = walk(destination,
                     time + travel_time, rate, released + rate * travel_time,
                     destinations_new)
@@ -119,15 +103,137 @@ function M.solve_p1(lines)
     return answer
 end
 
-function M.solve_p2(lines)
+function table.copy(e)
+    if type(e) == 'table' then
+        local e_new = {}
+        for k, v in pairs(e) do
+            e_new[k] = table.copy(v)
+        end
+        setmetatable(e_new, getmetatable(e))
+        return e_new
+    else
+        return e
+    end
 end
 
-function M.test() end
+local function set_to_string(set)
+    local list = {}
+    for x, _ in pairs(set) do
+        table.insert(list, x)
+    end
+    table.sort(list)
+    local result = ""
+    if #list > 0 then
+        result = result .. list[1]
+        for i = 2, #list do
+            result = result .. ',' .. list[i]
+        end
+    end
+    return result
+end
 
-M = {
-    M.solve_p1,
-    M.solve_p2,
-}
+local function get_set_size(set)
+    local count = 0
+    for _, _ in pairs(set) do
+        count = count + 1
+    end
+    return count
+end
+
+local function is_disjoint(set1, set2)
+    assert(type(set1) == 'table' and type(set2) == 'table')
+    for k, _ in pairs(set1) do
+        if set2[k] then
+            return false
+        end
+    end
+    for k, _ in pairs(set2) do
+        if set1[k] then
+            return false
+        end
+    end
+    return true
+end
+
+local function string_to_set(str)
+    local result = {}
+    for _, e in ipairs(util.split_string(str, ',')) do
+        result[e] = true
+    end
+    return result
+end
+
+function M.solve_p2(lines)
+    local valves, tunnels = parse(lines)
+    local destinations = get_nonzero_valves(valves)
+    local distances = calculate_distances(tunnels)
+
+    -- Calculate best achievable venting for each given set of visited
+    local results = {}
+    setmetatable(results, {
+        __index = function(tbl, k)
+            return rawget(tbl, tostring(k))
+        end,
+        __newindex = function(tbl, k, v)
+            rawset(tbl, tostring(k), v)
+        end,
+    })
+    local function walk(node, remaining, visited, vented)
+        local best = results[visited] or 0
+        results[visited] = math.max(best, vented)
+
+        for destination, _ in pairs(destinations) do
+            if not visited[destination] then
+                local travel_time = distances[node][destination] + 1 -- Travel + opening valve
+                local remaining_new = remaining - travel_time
+                if remaining_new >= 0 then
+                    local visited_new = table.copy(visited)
+                    visited_new[destination] = true
+                    walk(destination, remaining_new, visited_new, vented + remaining_new * valves[destination])
+                end
+            end
+        end
+    end
+
+    local visited = {}
+    setmetatable(visited, {
+        __tostring = function(self)
+            return set_to_string(self)
+        end,
+    })
+    walk('AA', 26, visited, 0)
+
+    local sum = 0
+    for s1, f1 in pairs(results) do
+        for s2, f2 in pairs(results) do
+            -- Slow and yucky to convert to string, but hey...
+            if is_disjoint(string_to_set(s1), string_to_set(s2)) then -- Visit sets does not overlap
+                sum = math.max(sum, f1 + f2)
+            end
+        end
+    end
+
+    return sum
+end
+
+function M.test()
+    print('bit', vim.inspect(bit.lshift(1, 31)))
+
+    print('set', vim.inspect(string_to_set('AA,BB,CC,DD')))
+    print('disjoint', is_disjoint(string_to_set('AA,BB,CC,DD'), string_to_set('BB')))
+
+    local a = { a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8, i = 9, j = 10, k = 11, l = 12, m = 13 }
+    local count = 0
+    for _, _ in pairs(a) do
+        for _, _ in pairs(a) do
+            count = count + 1
+        end
+    end
+    assert(count == (13 * 13))
+end
+
+M[1] = M.solve_p1
+M[2] = M.solve_p2
 
 return M
 
