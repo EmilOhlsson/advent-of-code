2f61809146ed03f221cbd64e83cbea0f715f1cd0
Emil Ohlsson <kotte@kottland.net>
2025-02-01 13:27:33 +0100

    stab at v2

--- python/2024-04-ceres-search/ceres.py
+++ python/2024-04-ceres-search/ceres.py
@@ -22,7 +22,19 @@ def xmas_at(pos: postype, ch: containertype) -> int:
     return sum([c in xmas for c in candidates])
 
 
+@typechecked
+def cross_mas_at(pos: postype, ch: containertype) -> int:
+    r, c = pos
+
+    candidates = [
+            f'{ch[(r-1,c-1)]}{ch[pos]}{ch[(r+1,c+1)]}',
+            f'{ch[(r+1,c-1)]}{ch[pos]}{ch[(r-1,c+1)]}', ]
+    xmas = { 'MAS', 'SAM' }
+    return 1 if all(cnd in xmas for cnd in candidates) else 0
+
 def main():
+    # TODO: Load default input path from env, and set up default env in direnv
+    # TODO: Set up python project for advent of code solutions
     parser = ArgumentParser(prog='ceres')
     parser.add_argument('-f', '--file', nargs='?', help='Input file',
                         type=Path, default='../../inputs/2024/04.txt')
@@ -34,12 +46,15 @@ def main():
             chars.update({ (row, col): ch for col, ch in
                           enumerate(line.strip()) })
 
-    count: int = 0
+    count_v1: int = 0
+    count_v2: int = 0
     key_set = set(chars.keys())
     for pos in key_set:
-        count += xmas_at(pos, chars)
+        count_v1 += xmas_at(pos, chars)
+        count_v2 += cross_mas_at(pos, chars)
     
-    print(f'Counted {count} XMAS')
+    print(f'Counted {count_v1} XMAS')
+    print(f'Counted {count_v2} CROSSMAS')
 
 
 if __name__ == '__main__':
