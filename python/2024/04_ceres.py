ad5ffd7a31e1e25d6e6cb04b2db73f19f7bfdba6
Emil Ohlsson <kotte@kottland.net>
2025-02-03 07:29:36 +0100

    Restructure python setup

--- python/2024/04_ceres.py
+++ python/2024/04_ceres.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+"""Solution for Advent of code 2024-04"""
+
 from argparse import ArgumentParser
 from collections import defaultdict
+from os import getenv
 from pathlib import Path
 
 from typeguard import typechecked
@@ -26,11 +29,24 @@ def xmas_at(pos: Pos, ch: Map) -> int:
     return sum(c in xmas for c in candidates)
 
 
+@typechecked
+def cross_mas_at(pos: Pos, ch: Map) -> int:
+    """Check if there is a cross-mas at `pos`"""
+    r, c = pos
+
+    candidates = [
+            f'{ch[(r-1, c-1)]}{ch[pos]}{ch[(r+1, c+1)]}',
+            f'{ch[(r+1, c-1)]}{ch[pos]}{ch[(r-1, c+1)]}', ]
+    xmas = {'MAS', 'SAM'}
+    return 1 if all(cnd in xmas for cnd in candidates) else 0
+
+
 def main():
     """Main function"""
+    input_dir = getenv('AOC_INPUT') or '../../inputs'
     parser = ArgumentParser(prog='ceres')
     parser.add_argument('-f', '--file', nargs='?', help='Input file',
-                        type=Path, default='../../inputs/2024/04.txt')
+                        type=Path, default=input_dir + '/2024/04.txt')
     args = parser.parse_args()
 
     chars: Map = defaultdict(lambda: '.')
@@ -39,12 +55,15 @@ def main():
             chars.update({(row, col): ch for col, ch in
                           enumerate(line.strip())})
 
-    count: int = 0
+    count_v1: int = 0
+    count_v2: int = 0
     key_set = set(chars.keys())
     for pos in key_set:
-        count += xmas_at(pos, chars)
+        count_v1 += xmas_at(pos, chars)
+        count_v2 += cross_mas_at(pos, chars)
 
-    print(f'Counted {count} XMAS')
+    print(f'Counted {count_v1} XMAS')
+    print(f'Counted {count_v2} CROSSMAS')
 
 
 if __name__ == '__main__':
