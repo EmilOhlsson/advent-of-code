51b9b257d97b7a84897c6a9dc58720d5e49dfa4b
Emil Ohlsson <kotte@kottland.net>
2025-02-18 21:43:14 +0100

    Not really working solution for part 2...

--- python/src/2024/aoc06_guard_gallivant.py
+++ python/src/2024/aoc06_guard_gallivant.py
@@ -3,21 +3,118 @@
 """Solution for Advent of code 2024-06."""
 
 from argparse import ArgumentParser
+from collections import defaultdict
 from os import getenv
 from pathlib import Path
 
 Pos = tuple[int, int]
+Step = tuple[int, int]
 Map = dict[Pos, str]
 
 
-def turn(direction):
+def rotate(direction: Step) -> Step:
     """Turn 90 degrees clockwise."""
     return (direction[1], -direction[0])
 
 
-def step(pos, delta):
+def step(pos: Pos, delta: Step) -> Pos:
     """Add a delta to pos."""
     return (pos[0] + delta[0], pos[1] + delta[1])
+
+
+class PuzzleMap:
+    """Solver class."""
+
+    UP: Step = (-1, 0)
+
+    def __init__(self, input_file):
+        """Parse input file into a map and starting position."""
+        self.puzzle_map: Map = {}
+        with open(input_file, encoding='utf-8') as file:
+            for row, line in enumerate(file):
+                for col, char in enumerate(line.strip()):
+                    if char == '^':
+                        self.guard_pos = (row, col)
+
+                    if char == '#':
+                        self.puzzle_map[(row, col)] = char
+                    else:
+                        self.puzzle_map[(row, col)] = ' '
+
+    def solve_p1(self) -> int:
+        """Simply walk through map, keeping track of visited positions."""
+        visited: set[Pos] = set()
+        delta = self.UP  # Direction
+        pos = self.guard_pos
+        while pos in self.puzzle_map:
+            # If there is an obstacle in the next position, turn
+            # until path is clear
+            pos_next = step(pos, delta)
+            while self.puzzle_map.get(pos_next, '') == '#':
+                delta = rotate(delta)
+                pos_next = step(pos, delta)
+
+            visited.add(pos)
+            pos = pos_next
+
+        return len(visited)
+
+    def check_for_loop(self, pos: Pos, delta: Step, already_visited) -> bool:
+        """Check if start traveling here would result in loop."""
+        visited: defaultdict[Pos, set[Pos]] = defaultdict(set) | already_visited
+
+        while pos in self.puzzle_map:
+            # Check if we've already been here, traveling in same direction
+            if delta in visited[pos]:
+                return True
+
+            # Check if next position has an obstacle, if so: turn
+            pos_next = step(pos, delta)
+            while self.puzzle_map.get(pos_next, '') == '#':
+                delta = rotate(delta)
+                pos_next = step(pos, delta)
+
+            # Consider this position visited
+            visited[pos].add(delta)
+            pos = pos_next
+
+        return False
+
+    def solve_p2(self):
+        """Walk through map, and in each point consider possible blockade."""
+        pos = self.guard_pos
+        visited: defaultdict[Pos, set[Pos]] = defaultdict(set)
+        delta = self.UP
+        loop_points: set[Pos] = set()
+
+        while pos in self.puzzle_map:
+            # If there is an obstacle in the next position, turn
+            # until path is clear
+            pos_next = step(pos, delta)
+            while self.puzzle_map.get(pos_next, '') == '#':
+                delta = rotate(delta)
+                pos_next = step(pos, delta)
+
+            # What would happen if we place an obstacle in front of us. No need
+            # to attempt to place obstacle if we've already passed the candidate
+            # position of pos_next
+            if pos_next in self.puzzle_map and len(visited[pos_next]) == 0:
+                assert self.puzzle_map[pos_next] != '#'
+
+                if rotate(delta) in visited[pos]:
+                    # No need to do full check
+                    loop_points.add(pos_next)
+                else:
+                    self.puzzle_map[pos_next] = '#'
+                    if self.check_for_loop(pos, rotate(delta), visited):
+                        loop_points.add(pos_next)
+                    self.puzzle_map[pos_next] = ' '
+
+            # Mark current position as visited, and logging direction
+            visited[pos].add(delta)
+            pos = pos_next
+
+        return len(loop_points)
 
 
 def main():
@@ -27,33 +124,10 @@ def main():
     parser.add_argument('-f', '--file', nargs='?', help='Input file',
                         type=Path, default=input_dir + '/2024/06.txt')
     args = parser.parse_args()
+    puzzle_map = PuzzleMap(args.file)
 
-    puzzle_map: Map = {}
-    with open(args.file, encoding='utf-8') as file:
-        for row, line in enumerate(file):
-            for col, char in enumerate(line.strip()):
-                if char == '^':
-                    guard_pos = (row, col)
-                    char = '.'
-                puzzle_map[(row, col)] = char
-
-    visited: set[Pos] = set()
-    dx: tuple[int, int] = (-1, 0)  # (row, col)
-
-    next_pos = step(guard_pos, dx)
-    while next_pos in puzzle_map:
-        visited.add(guard_pos)
-        # If there is an obstacle in the next position, turn
-        # until path is clear
-        while puzzle_map[next_pos] == '#':
-            dx = turn(dx)
-            next_pos = step(guard_pos, dx)
-
-        guard_pos = step(guard_pos, dx)
-        next_pos = step(guard_pos, dx)
-    visited.add(guard_pos)
-
-    print(f'Visited {len(visited)}')
+    print(f'Visited {puzzle_map.solve_p1()}')      # 5030
+    print(f'Loop points {puzzle_map.solve_p2()}')  # 278, 422 is too low, 4484 is too high
 
 
 if __name__ == '__main__':
